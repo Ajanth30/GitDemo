2a2cf348038b89a4606d2f436d56ba24fbc768d8
Ajanth <110395206+Ajanth30@users.noreply.github.com>
2022-10-08 12:28:01 +0530

    Update GitDemo.java

--- src/main/java/GitDemo.java
+++ src/main/java/GitDemo.java
@@ -1,6 +1,7 @@
 public class GitDemo {
     public  static void method(){
         System.out.println("Hello Git");
-        System.out.println("Changed");
+        System.out.println("Changed in github");
+       
     }
 }
